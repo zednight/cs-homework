fee93f783d7d8f40f1948e3175bf2e529406ec66
PinkRino <iltrony@gmail.com>
2025-11-29 14:20:26 +0300

    lab 3.2

---
+++ Lab3.2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Lab3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int j = 1;
+            while(j < 1000)
+            {
+                Console.WriteLine(j);
+                j += i;
+                i = j - i;
+            }
+        }
+    }
+}
