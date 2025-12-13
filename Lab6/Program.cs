0a783fe8300eb037ebd03d5fbea68d63475591a7
PinkRino <iltrony@gmail.com>
2025-12-13 12:42:30 +0300

    lab 6

---
+++ Lab6/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Lab6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DrawScene([
+                new Point(4, 5, "red"),
+                new Line(2, 2, 3, 3, "yellow"),
+                new Circle(3, 8, 13, "blue")
+            ]);
+        }
+
+        public static void DrawScene(Shape[] scene)
+        {
+            foreach(Shape shape in scene)
+            {
+                shape.Draw();
+            }
+        }
+
+    }
+}
---
+++ Lab6/Shape.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab6
+{
+    public abstract class Shape
+    {
+        public string Color { get; set; }
+        public abstract Shape Draw();
+        public const string Def_color = "black";
+        public Shape(string color)
+        {
+            this.Color = color;
+        }
+        public string DrawColor()
+        {
+            return $" / color : {this.Color}";
+        }
+    }
+}
