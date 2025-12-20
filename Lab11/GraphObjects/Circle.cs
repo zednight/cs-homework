97fb14a66f5efb4b5c2a9bc093a1185790e25753
PinkRino <iltrony@gmail.com>
2025-12-20 12:43:01 +0300

    Lab11

---
+++ Lab11/GraphObjects/Circle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Lab11.GraphObjects
+{
+    public class Circle : Shape
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        private double radius;
+        public double Radius
+        {
+            get => radius;
+            set
+            {
+                if (value > 0)
+                {
+                    this.radius = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException($"Не корректное значение радиуса ({value} меньше или равно 0)");
+                }
+            }
+        }
+        public Circle(int x = 0, int y = 0, double radius = 5.0, string color = Shape.Def_color) : base(color)
+        {
+            X = x;
+            Y = y;
+            Radius = radius;
+        }
+        public override Circle Draw()
+        {
+            Console.WriteLine($"Circle - X: {this.X}, Y: {this.Y}, Radius : {this.Radius}{this.DrawColor()}");
+            return this;
+        }
+        public Circle MoveBy(int x, int y)
+        {
+            this.X += x;
+            this.Y += y;
+            return this;
+        }
+        public Circle Scale(double scale)
+        {
+            this.Radius *= scale;
+            return this;
+        }
+    }
+
+}
---
+++ Lab11/Program.cs
@@ -0,0 +1,29 @@
+﻿using Lab11.GraphObjects;
+
+namespace Lab11
+{
+    internal class Program
+    {
+        const string saveFileName = @"..\..\..\result\scene.json";
+        const string loadFileName = @"..\..\..\result\scene.json";
+        static void Main(string[] args)
+        {
+            Scene sc = new Scene();
+
+            sc
+                .Add([
+                    new Point(4, 5, "red"),
+                    new Line(2, 2, 3, 3, "yellow"),
+                    new Circle(3, 8, 13, "blue")
+                ])
+                .Scale(3)
+                .DrawScene("before clear")
+                //.Save(saveFileName)
+                .Clear()
+                .DrawScene("after clear");
+
+            Scene sc2 = Scene.Load(loadFileName);
+            sc2.DrawScene("draw sc2");
+        }
+    }
+}
